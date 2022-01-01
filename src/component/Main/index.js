d3d783a67dd95edca7226fee9f8824f557586b1d
enesberk <180403049@ogr.gelisim.edu.tr>
2022-01-01 19:34:23 +0300

    mainscreen-navbar

--- src/component/Main/index.js
+++ src/component/Main/index.js
@@ -1,26 +1,48 @@
 import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './main.css';
+import { contains } from 'jquery';
 
 export default class Main extends Component {
   render() {
     return (
+
+
+
       <header>
-        <nav className="navbar navbar-light bg-light">
-          <div className="container-fluid">
-            <img src={'logo.png'} style={{ width: 200 }} />
-            <form className="d-flex">
-              <button className="btn btn-outline-success" type="submit">Giriş Yap</button>
+        <div className="border"></div>   {/*top-border*/}
+
+        <div id="nav">
+          <div class="logosearchbutt">   {/*logo - search - login/register buttons*/}
+            <a href="#"><img src={'logo.png'} id="logo" /></a>
+            <form class="searchb">
+              <input type={"text"} placeholder="başlık, #entry, @yazar" name="search" />
+              <img src={"srcb.png"} />
+
+
+
             </form>
+            <div class="loginregister">
+              <a href="#">register</a>
+              <a href="#">login</a>
+            </div>
           </div>
-        </nav>
-        <ul>
-          <li><a class="active" href="#home">Home</a></li>
-          <li><a href="#news">News</a></li>
-          <li><a href="#contact">Contact</a></li>
-          <li><a href="#about">About</a></li>
-        </ul>
-      </header>
+          <ul>
+            <li><a href="#">gündem</a></li>
+            <li><a href="#">debe</a></li>
+            <li><a href="#">sorunsallar</a></li>
+            <li><a href="#">#spor</a></li>
+            <li><a href="#">#ilişkiler</a></li>
+            <li><a href="#">#siyaset</a></li>
+            <li><a href="#" id="nokta">...</a></li>
+          </ul>
+
+        </div>
+
+      </header> 
+
+      
+
     )
   }
 }
