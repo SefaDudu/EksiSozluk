726e886bba60ea9b05dd702def97afe1df4fb08f
enesberk <180403049@ogr.gelisim.edu.tr>
2022-01-03 22:48:01 +0300

    mainscreen-navbar added-fixed

--- src/component/Main/index.js
+++ src/component/Main/index.js
@@ -8,41 +8,72 @@ export default class Main extends Component {
     return (
 
 
-
-      <header>
-        <div className="border"></div>   {/*top-border*/}
-
-        <div id="nav">
-          <div class="logosearchbutt">   {/*logo - search - login/register buttons*/}
-            <a href="#"><img src={'logo.png'} id="logo" /></a>
-            <form class="searchb">
-              <input type={"text"} placeholder="başlık, #entry, @yazar" name="search" />
-              <img src={"srcb.png"} />
+      <>
+        <div className="bordercontainer1"></div>   {/*top-border*/}
+        <div>
 
 
+          <div id="nav">
+            <div className="logosearchbutt">   {/*logo - search - login/register buttons*/}
+              <a href="#"><img src={'logo.png'} id="logo" /></a>
+              <form className="searchb">
+                <input type={"text"} placeholder="başlık, #entry, @yazar" name="search" />
+                <img src={"srcb.png"} />
 
-            </form>
-            <div class="loginregister">
-              <a href="#">register</a>
-              <a href="#">login</a>
+
+
+              </form>
+              <div className="loginregister">
+                <a href="#">register</a>
+                <a href="#">login</a>
+              </div>
             </div>
+            <ul>
+              <li><a href="#" id="g">gündem</a></li>
+              <li><a href="#">debe</a></li>
+              <li><a href="#">sorunsallar</a></li>
+              <li><a href="#">#spor</a></li>
+              <li><a href="#">#ilişkiler</a></li>
+              <li><a href="#">#siyaset</a></li>
+              <li><a href="#" id="nokta">...</a></li>
+            </ul>
+
           </div>
-          <ul>
-            <li><a href="#">gündem</a></li>
-            <li><a href="#">debe</a></li>
-            <li><a href="#">sorunsallar</a></li>
-            <li><a href="#">#spor</a></li>
-            <li><a href="#">#ilişkiler</a></li>
-            <li><a href="#">#siyaset</a></li>
-            <li><a href="#" id="nokta">...</a></li>
-          </ul>
 
         </div>
+        <div className="bordercontainer"></div>
+        <div className="Containerr">
+          {/*sidebar*/}
+          <div className="sidebar">
 
-      </header> 
+            <div className="baslik">
+              <span>gündem</span>
+              <img src={"settings.png"} />
+            </div>
 
-      
+            <div className="baslikicerik">
 
+              <ul>
+                <li><a href="#">Dikkat bu yazı çok uzun bir deneme yazısıdır lorem ipsum dolar sit amet</a><small>20000</small></li>
+                <li><a href="#1">x</a><small>200</small></li>
+                <li><a href="#2">x</a><small>200</small></li>
+                <li><a href="#3">x</a><small>200</small></li>
+                <li><a href="#4">x</a><small>200</small></li>
+                <li><a href="#5">x</a><small>200</small></li>
+              </ul>
+            </div>
+
+
+          </div>
+
+          {/*içerik kısmı*/}
+          <div className="contents">
+           <h1>içerik kısmı</h1> 
+          </div>
+        </div>
+
+
+      </>
     )
   }
 }
